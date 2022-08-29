b23bcc0eb3a60b8c97e50a4b95275409d3aecf07
Camilla Poushyk <camilla.poushyk@gmail.com>
2022-08-29 11:06:13 -0400

    commit tree 08077a77e7300a96a9db527a24987b29b4f4ffa6

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+
+    badges = []
+    names.each do |name|
+        badges << "Hello, my name is #{name}."
+    end
+
+    badges
+end
+
+def assign_rooms(names)
+    rooms = []
+    roomI = 0
+    names.each do |name|
+        rooms << "Hello, #{name}! You'll be assigned to room #{roomI += 1}!"
+    end
+    rooms
+end
+
+def printer(names)
+    badges = batch_badge_creator names
+    rooms = assign_rooms names
+
+    badges.each do |badge|
+        puts badge
+    end
+
+    rooms.each do |room|
+        puts room
+    end
+end
